e782ec76edb0fd0cfa0f951c7cf370d86001f4d5
Krochi <77779207+Krochi@users.noreply.github.com>
2024-06-18 07:11:31 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,12 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+empty_current = 0
+
+while empty_current < len(my_list):
+    new_list = my_list[empty_current]
+    if new_list > 0:
+        print(new_list)
+        empty_current += 1
+    elif new_list <= 0:
+        empty_current += 1
+    else:
+        break
